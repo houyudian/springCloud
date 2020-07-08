dc3bd7d681f383c581fbc2352f497ca2d3b91266
huahua <3891688944@qq.com>
2020-07-08 19:04:47 +0800

    eureka

---
+++ register-eureka/src/test/java/com/fh/shop/registereureka/RegisterEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.shop.registereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
